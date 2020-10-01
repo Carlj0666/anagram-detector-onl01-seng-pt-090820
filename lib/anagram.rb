8c4098864b6649b481e227ff662c42435eb6b123
Carl J <carlj1881@yahoo.com>
2020-10-01 04:50:39 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -15,7 +15,7 @@ class Anagram
     match_array = []
      #binding.pry
     
-    if array.sort == word.split("").sort
+    if array.sort & word.split("").sort
       match_array << word
       
     end
