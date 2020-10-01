c60dcb8b6c624f98787ea4e9743406c05d14bed7
Carl J <carlj1881@yahoo.com>
2020-10-01 05:01:15 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -17,12 +17,12 @@ class Anagram
     array.each do |anagram|
       if anagram.select(new_word)
       match_array << new_word
+    end
   end
 
  
   
 end
 
-	
-# John Chriest A FEW SECONDS AGO
+
 # We need to iterate over the array and select for any word that is the samesplit and sorted as the @name is split and sorted
