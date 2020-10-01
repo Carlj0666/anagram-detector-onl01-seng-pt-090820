382321b5063fde9cf1700fc6049e6eb22cce1d80
Carl J <carlj1881@yahoo.com>
2020-10-01 04:42:54 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+
+require"pry"
+
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+
+  
+
+  def match(array)
+    match_array = []
+     #binding.pry
+    
+    array.split("").sort == word.split("").sort
+      match_array << word
+      match_array
+   end
+
+ 
+  
+end
